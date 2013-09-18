e918959825b803efe51b4c33d76a91acf852832d
Boris Filipov <obiwonn@gmail.com>
2013-09-18 14:20:06 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var response = require(process.cwd()+"/node_modules/express").response
+
+var expressSend = module.exports = function(c, next){
+  if(c.res.body) {
+    var oldProto = c.res.__proto__;
+    c.res.app = {settings: {}, get:function(){}}
+    c.res.req = c.req
+    c.res.__proto__ = response
+    if(c.res.code)
+      c.res.send(c.res.code, c.res.body)
+    else
+      c.res.send(c.res.body)
+    delete c.res.app
+    delete c.res.req
+    c.res.__proto__ = oldProto
+  }
+  next()
+}
